3b9ffb28ff9cda4ebf6e3d908f22c779d0254d1c
patrick0705 <patrickschwarzmann87@gmail.com>
2023-03-22 17:54:38 +0100

    versuch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,8 @@ import BarcodeScan from '../components/BarcodeScan.vue'
 </script>
 
 <template>
-  <main>
      <BarcodeScan @decode="onDecode" @loaded="onLoaded"></BarcodeScan>
-  </main>
 </template>
+
+<style scoped>
+</style>

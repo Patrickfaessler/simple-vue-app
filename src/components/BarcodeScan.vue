f0299a666be4e0bfd0b551a94dafcd8d51e9bd65
patrick0705 <patrickschwarzmann87@gmail.com>
2023-03-22 19:43:33 +0100

    Artikel eigefügt

--- src/components/BarcodeScan.vue
+++ src/components/BarcodeScan.vue
@@ -7,15 +7,16 @@
     <button :style="{ backgroundColor: foundCode ? 'green' : 'red' }">
       {{ foundCode ? 'Code gefunden!' : 'Suche nach Code...' }}
     </button>
+    <Artikel :barcodeText="decodedText" v-if="foundCode"></Artikel>
   </div>
 </template>
 
 <script setup>
 import { ref, computed } from "vue";
 import { StreamBarcodeReader } from "vue-barcode-reader";
+import Artikel from "./Artikel.vue";
 
 const decodedText = ref("");
-const noCodeFound = ref(false);
 
 const onLoaded = () => {
   console.log("loaded");
@@ -23,8 +24,8 @@ const onLoaded = () => {
 
 const onDecode = (text) => {
   decodedText.value = text;
-  noCodeFound.value = false;
 };
 
 const foundCode = computed(() => decodedText.value !== "");
+
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,6 @@ import BarcodeScan from '../components/BarcodeScan.vue'
 
 <template>
      <BarcodeScan @decode="onDecode" @loaded="onLoaded"></BarcodeScan>
-     <v-button @click="">Scan</v-button>
 </template>
 
 <style scoped>

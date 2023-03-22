5c50ee9fd1a7596ebbe479a74445ad69d020d750
patrick0705 <patrickschwarzmann87@gmail.com>
2023-03-22 18:46:08 +0100

    Merge

--- src/components/BarcodeScan.vue
+++ src/components/BarcodeScan.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import { StreamBarcodeReader } from "vue-barcode-reader";
 
 const decodedText = ref("");
+const noCodeFound = ref(false);
 
 const onLoaded = () => {
   console.log("loaded");
@@ -10,11 +11,15 @@ const onLoaded = () => {
 
 const onDecode = (text) => {
   decodedText.value = text;
+  noCodeFound.value = false;
 };
+
+const foundCode = computed(() => decodedText.value !== "");
 </script>
 
 <template>
   <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+  <button :style="{backgroundColor: foundCode ? 'green' : 'red'}">{{ foundCode ? 'Code gefunden' : 'Suche nach Code...' }}</button>
   <h2>{{ decodedText }}</h2>
 </template>
 
